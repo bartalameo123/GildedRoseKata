0f38bd684584f6a9b47de63edee3a032d9df2364
taba <artur.basiul@bite.lt>
2019-02-04 11:19:44 +0200

    Removed sulfuras from processing, removed negation

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -15,14 +15,11 @@ class GildedRose {
 
    //     Arrays.asList(items).stream().
         for (Item item:items) {
-            if (!item.name.equals("Aged Brie")
-                    && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) { //do not apply to sulfuras
-                        item.quality = item.quality - 1;
-                    }
-                }
-            } else {
+            if (item.name.equals("Sulfuras, Hand of Ragnaros")){ //do not apply to sulfuras
+                continue;
+            }
+            if (item.name.equals("Aged Brie")
+                    || item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (item.quality < 50) {  //max quality is 50
                     item.quality = item.quality + 1;  //increae for brie and passes
 
@@ -40,26 +37,25 @@ class GildedRose {
                         }
                     }
                 }
+            } else {
+
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
+                }
             }
 
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                item.sellIn = item.sellIn - 1;
-            }
+            item.sellIn = item.sellIn - 1;
 
             if (item.sellIn < 0) { //if sell day passed quality degrades twice as fast
-                if (!item.name.equals("Aged Brie")) {
-                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.quality > 0) { //never negative
-                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) { //do not apply on sulfuras
-                                item.quality = item.quality - 1; //second decrease
-                            }
-                        }
-                    } else {
-                        item.quality = item.quality - item.quality;
-                    }
-                } else {
+                if (item.name.equals("Aged Brie")) {
                     if (item.quality < 50) { //aged brie increases in quality
                         item.quality = item.quality + 1;
+                    }
+                } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    item.quality = 0;
+                } else {
+                    if (item.quality > 0) { //never negative
+                        item.quality = item.quality - 1; //second decrease
                     }
                 }
             }
--- src/main/java/com/gildedrose/entity/BackStageItem.java
+++ src/main/java/com/gildedrose/entity/BackStageItem.java
@@ -2,8 +2,14 @@ package com.gildedrose.entity;
 
 public class BackStageItem extends Item{
 
-    public BackStageItem(){
-        super();
+    public String name;
+    public int sellIn;
+    public int quality;
+
+    public BackStageItem(String name, int sellIn, int quality){
+        super(name,sellIn,quality);
     }
 
+
+    //custom backstage methods
 }
