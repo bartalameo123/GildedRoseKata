3aaf56abcb3b56188c5f45bed1c1aa254184daeb
taba <artur.basiul@bite.lt>
2019-02-04 11:46:27 +0200

    extract methods

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -7,6 +7,9 @@ import java.util.Arrays;
 class GildedRose {
     Item[] items;
 
+    private final static int MAX_QUALITY = 50;
+    private final static int MIN_QUALITY = 0;
+
     public GildedRose(Item[] items) {
         this.items = items;
     }
@@ -20,45 +23,76 @@ class GildedRose {
             }
             if (item.name.equals("Aged Brie")
                     || item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality < 50) {  //max quality is 50
-                    item.quality = item.quality + 1;  //increae for brie and passes
+                if (itemQualityIsNotMaximal(item.quality)) {  //max quality is 50
+                    increaseQuality(item);  //increae for brie and passes
 
                     if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1; //+2 if <=10 days
+                        if (backstageQualityDoubles(item.sellIn)) {
+                            if (itemQualityIsNotMaximal(item.quality)) {
+                                increaseQuality(item); //+2 if <=10 days
                             }
                         }
 
-                        if (item.sellIn < 6) { //+3 if less than 6 days
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
+                        if (backstageQualityTriples(item.sellIn)) { //+3 if less than 6 days
+                            if (itemQualityIsNotMaximal(item.quality)) {
+                                increaseQuality(item);
                             }
                         }
                     }
                 }
-            } else {
-
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1;
-                }
+            } else if (itemQualityIsNotMinimal(item.quality)) {
+                decreseQuality(item);
             }
 
-            item.sellIn = item.sellIn - 1;
+            decreaseSellTime(item);
 
-            if (item.sellIn < 0) { //if sell day passed quality degrades twice as fast
+
+
+            if (sellDayHasPassed(item.sellIn)) { //if sell day passed quality degrades twice as fast
                 if (item.name.equals("Aged Brie")) {
-                    if (item.quality < 50) { //aged brie increases in quality
-                        item.quality = item.quality + 1;
+                    if (itemQualityIsNotMaximal(item.quality)) { //aged brie increases in quality
+                        increaseQuality(item);
                     }
                 } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                     item.quality = 0;
                 } else {
-                    if (item.quality > 0) { //never negative
-                        item.quality = item.quality - 1; //second decrease
+                    if (itemQualityIsNotMinimal(item.quality)) { //never negative
+                        decreseQuality(item); //second decrease
                     }
                 }
             }
         }
+    }
+
+    private boolean backstageQualityDoubles(int sellIn){
+        return sellIn < 11;
+    }
+
+    private boolean itemQualityIsNotMaximal(int quality){
+        return quality < MAX_QUALITY;
+    }
+
+    private boolean itemQualityIsNotMinimal(int quality){
+        return quality > MIN_QUALITY;
+    }
+
+    private boolean sellDayHasPassed(int sellIn){
+        return sellIn < 0;
+    }
+
+    private boolean backstageQualityTriples(int sellIn){
+        return sellIn < 6;
+    }
+
+    private void decreseQuality(Item item){
+        item.quality--;
+    }
+
+    private void increaseQuality(Item item){
+        item.quality++;
+    }
+
+    private void decreaseSellTime(Item item){
+        item.sellIn--;
     }
 }
